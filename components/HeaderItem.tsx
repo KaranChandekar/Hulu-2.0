1c1c248c94ecd6c90b3c99e4172b174b8d21c65d
Karan <karanchandekar431@gmail.com>
2023-01-26 17:58:11 +0530

    update headerItem

--- components/HeaderItem.tsx
+++ components/HeaderItem.tsx
@@ -1,16 +1,14 @@
-import React from "react";
+import React, { InsHTMLAttributes } from "react";
 
-type Props = {};
-
-const HeaderItem = ({ Icon, title }: Props) => {
+function HeaderItem({ Icon, title }: any) {
   return (
-    <div className="group cursor-pointer flex flex-col items-center w-14 sm:w-16 hover:text-white">
+    <div className="flex flex-col items-center cursor-pointer group w-14 sm:w-16 hover:text-white">
       <Icon className="h-8 mb-1 group-hover:animate-bounce" />
       <p className="tracking-widest opacity-0 group-hover:opacity-100">
         {title}
       </p>
     </div>
   );
-};
+}
 
 export default HeaderItem;
